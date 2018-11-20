ae0d3d4b3fb57a599fe683676fb758da642ff93e
Fenghao Chen <fchen1@linkedin.com>
2018-11-19 17:43:15 -0800

    lix cleanup

--- src/main/java/fchen/lix/example/Work.java
+++ src/main/java/fchen/lix/example/Work.java
@@ -3,25 +3,13 @@ package fchen.lix.example;
 import fchen.lix.common.LixAnnotation;
 import fchen.lix.common.LixDecorator;
 
-
 public class Work {
 
-  public void doSomething() {
-    LixDecorator.decorateMethods(this.getClass(), Work::enabled, Work::control, Work::other);
-  }
+    public void doSomething() {
+        enabled();
+    }
 
-  @LixAnnotation(lixKey = "foo.bar", treatment = "enabled")
-  public void enabled() {
-    System.out.println("treatment enabled");
-  }
-
-  @LixAnnotation(lixKey = "foo.bar", treatment = "control")
-  public void control() {
-    System.out.println("treatment control");
-  }
-
-  @LixAnnotation(lixKey = "foo.bar", treatment = "other")
-  public void other() {
-    System.out.println("treatment other");
-  }
+    public void enabled() {
+        System.out.println("treatment enabled");
+    }
 }
